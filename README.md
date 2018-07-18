88f07df5280219ffef0fa9b294aec1dc58aabedc
Kaz Utashiro <kaz@utashiro.com>
2018-07-18 15:16:35 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Text::ANSI::Fold - It's new $module
+
+# SYNOPSIS
+
+    use Text::ANSI::Fold;
+
+# DESCRIPTION
+
+Text::ANSI::Fold is ...
+
+# LICENSE
+
+Copyright (C) Kaz Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kaz Utashiro <kaz@utashiro.com>
---
+++ lib/Text/ANSI/Fold.pm
@@ -0,0 +1,39 @@
+package Text::ANSI::Fold;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Text::ANSI::Fold - It's new $module
+
+=head1 SYNOPSIS
+
+    use Text::ANSI::Fold;
+
+=head1 DESCRIPTION
+
+Text::ANSI::Fold is ...
+
+=head1 LICENSE
+
+Copyright (C) Kaz Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kaz Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
