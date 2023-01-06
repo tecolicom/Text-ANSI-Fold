22f353c0e97aa9e09695a65c1a359281cf795057
Kazumasa Utashiro <kaz@utashiro.com>
2023-01-06 11:09:27 +0900

    allow double backspace in run-in character

--- lib/Text/ANSI/Fold.pm
+++ lib/Text/ANSI/Fold.pm
@@ -435,7 +435,7 @@ sub fold {
 	while ($m > 0 and
 	       m{\A (?<color> ${color_re}*+)
 	            (?<runin> $prohibition_re{head} )
-		    ( \cH \g{runin} )* # multiple strike
+		    ( \cH{1,2} \g{runin} )* # multiple strike
 	            (?<reset> (?: $erase_re* $reset_re+ $erase_re* )? )
 	       }xp) {
 	    my $w = vwidth $+{runin};
